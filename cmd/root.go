c213a226bd8738428596436aa3b367978e14e556
Robin Yonge <robinfarrowyonge@gmail.com>
2019-02-25 13:30:22 +0000

    update cli to use a subcommand

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,21 @@
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
+    "fmt"
+    "io/ioutil"
+    "os"
 
-	"github.com/buger/jsonparser"
+    "github.com/buger/jsonparser"
 
-	"github.com/spf13/cobra"
+    "github.com/spf13/cobra"
 
-	"github.com/newscorp-ghfb/okta-kubectl-auth/pkg/okta"
+    "github.com/newscorp-ghfb/okta-kubectl-auth/pkg/okta"
 )
 
 type Flags struct {
 	Debug        bool
-	ClientID     string
-	ClientSecret string
+    ClientID     string
+    ClientSecret string
 	BaseDomain   string
 	BindAddr     string
 	InputConfig  string
@@ -27,8 +27,27 @@ var flags = &Flags{}
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "okta-kubectl-auth",
+	Use: "okta-kubectl-auth authorize",
 	Short: "Okta login helper for kubectl",
+	Example: `
+  To run with a config file:
+  okta-kubectl-auth authorize --config=/path/to/config/file.json
+
+  To run without a config file:
+  okta-kubectl-auth authorize --client-id=<id> --client-secret=<secret> --base-domain=<domain>
+
+  To automatically configure your kubeconfig supply the username and kubeconfig flags:
+  okta-kubectl-auth authorize --config=/path/to/config/file.json --username=<name> --kubeconfig=/path/to/kubeconfig`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Help()
+
+		return nil
+	},
+}
+
+var AuthCmd = &cobra.Command{
+	Use: "authorize",
+	Short: "Authenticates and optionally configures your kubeconfig",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		o := newOkta(flags)
 
@@ -37,7 +56,6 @@ var RootCmd = &cobra.Command{
 		}
 
 		return nil
-
 	},
 }
 
@@ -49,23 +67,24 @@ func Execute() {
 }
 
 func newOkta(flags *Flags) *okta.Okta {
-	o := okta.New(nil, flags.Debug)
-	if flags.ClientID == "" {
-	    o.ClientID, o.ClientSecret, o.BaseDomain = ParseConfig(flags.InputConfig, flags.Username)
-	} else {
-	    o.ClientID = flags.ClientID
-        o.ClientSecret = flags.ClientSecret
-        o.BaseDomain = flags.BaseDomain
-	}
-	o.BindAddr = flags.BindAddr
+    o := okta.New(nil, flags.Debug)
+    o.BindAddr = flags.BindAddr
 	o.Debug = flags.Debug
 	o.KubeConfig = flags.KubeConfig
 	o.Username = flags.Username
+
+    if flags.InputConfig != "" {
+    	o.ClientID, o.ClientSecret, o.BaseDomain = ParseConfig(flags.InputConfig, flags.Username)
+    } else {
+    	o.ClientID = flags.ClientID
+        o.ClientSecret = flags.ClientSecret
+        o.BaseDomain = flags.BaseDomain
+	}
+
 	return o
 }
 
 func ParseConfig(path string, username string) (string, string, string) {
-
 	jsonConfig, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
@@ -79,14 +98,15 @@ func ParseConfig(path string, username string) (string, string, string) {
 }
 
 func init() {
-	RootCmd.Flags().StringVar(&flags.ClientID, "client-id", "", "OAuth2 client ID of this application.")
-	RootCmd.Flags().StringVar(&flags.ClientSecret, "client-secret", "", "OAuth2 client secret of this application.")
+	RootCmd.AddCommand(AuthCmd)
+	AuthCmd.Flags().StringVar(&flags.ClientID, "client-id", "", "OAuth2 client ID of this application.")
+	AuthCmd.Flags().StringVar(&flags.ClientSecret, "client-secret", "", "OAuth2 client secret of this application.")
 
-	RootCmd.PersistentFlags().StringVar(&flags.BaseDomain, "base-domain", "", "URL of the OpenID Connect issuer.")
-	RootCmd.PersistentFlags().StringVar(&flags.BindAddr, "bind-addr", "127.0.0.1:8888", "HTTP address to listen at.")
-	RootCmd.PersistentFlags().BoolVar(&flags.Debug, "debug", false, "Raise log level to debug.")
+	AuthCmd.PersistentFlags().StringVar(&flags.BaseDomain, "base-domain", "", "URL of the OpenID Connect issuer.")
+	AuthCmd.PersistentFlags().StringVar(&flags.BindAddr, "bind-addr", "127.0.0.1:8888", "HTTP address to listen at.")
+	AuthCmd.PersistentFlags().BoolVar(&flags.Debug, "debug", false, "Raise log level to debug.")
 
-	RootCmd.PersistentFlags().StringVar(&flags.KubeConfig, "kubeconfig", "", "Path to the kubeconfig you want to update.")
-	RootCmd.PersistentFlags().StringVar(&flags.InputConfig, "config", "cluster_config.json", "Path to a json file containing the required keys/tokens. (see README)")
-	RootCmd.PersistentFlags().StringVar(&flags.Username, "username", "", "Username/cluster to use when setting credentials in the kubeconfig.")
+	AuthCmd.PersistentFlags().StringVar(&flags.KubeConfig, "kubeconfig", "", "Path to the kubeconfig you want to update.")
+	AuthCmd.PersistentFlags().StringVar(&flags.InputConfig, "config", "", "Path to a json file containing the required keys/tokens. (see README)")
+	AuthCmd.PersistentFlags().StringVar(&flags.Username, "username", "", "Username/cluster to use when setting credentials in the kubeconfig.")
 }
